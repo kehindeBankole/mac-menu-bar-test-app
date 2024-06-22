f62f7a35296eb8cbff3e9fa052046afd1f9ef316
kehindeBankole <bankolek1@gmail.com>
2024-06-22 20:20:02 +0100

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by Kehinde Bankole on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
